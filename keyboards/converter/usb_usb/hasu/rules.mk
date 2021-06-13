8333f319ec97fbeda1ee228fe8cfe3b1ca90522b
rmalcolm538 <rmalcolm538@gmail.com>
2021-06-13 18:32:47 -0400

    adding my keymap

--- keyboards/converter/usb_usb/hasu/rules.mk
+++ keyboards/converter/usb_usb/hasu/rules.mk
@@ -3,3 +3,11 @@ F_CPU = 16000000
 
 # Bootloader selection
 BOOTLOADER = atmel-dfu
+
+# Allow combos
+COMBO_ENABLE = yes
+
+COMMAND_ENABLE = no
+UNICODE_ENABLE = no
+SWAP_HANDS_ENABLE = no
+MOUSEKEY_ENABLE = no
--- keyboards/converter/usb_usb/rules.mk
+++ keyboards/converter/usb_usb/rules.mk
@@ -14,17 +14,19 @@ BOOTLOADER = caterina
 # Build Options
 #   comment out to disable the options.
 #
-#BOOTMAGIC_ENABLE = full  # Virtual DIP switch configuration
-#MOUSEKEY_ENABLE  = yes  # Mouse keys
-EXTRAKEY_ENABLE  = yes  # Audio control and System control
-#CONSOLE_ENABLE   = yes  # Console for debug
-#COMMAND_ENABLE   = yes  # Commands for debug and configuration
-#SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
-#NKRO_ENABLE      = yes  # USB Nkey Rollover - not yet supported in LUFA
-#BACKLIGHT_ENABLE = yes
+BOOTMAGIC_ENABLE = no # Virtual DIP switch configuration
+MOUSEKEY_ENABLE  = no # Mouse keys
+EXTRAKEY_ENABLE  = no # Audio control and System control
+CONSOLE_ENABLE   = no # Console for debug
+COMMAND_ENABLE   = no # Commands for debug and configuration
+SLEEP_LED_ENABLE = no # Breathing sleep LED during USB suspend
+NKRO_ENABLE      = no # USB Nkey Rollover - not yet supported in LUFA
+
 USB_HID_ENABLE   = yes
 CUSTOM_MATRIX    = yes
 
 SRC = custom_matrix.cpp
 
 DEFAULT_FOLDER = converter/usb_usb/hasu
+
+EXTRAFLAGS += -flto
