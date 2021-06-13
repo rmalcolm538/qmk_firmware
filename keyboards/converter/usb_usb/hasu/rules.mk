d04cb173ca0e6f20d189ef3c8c1efd0c375f63d3
rmalcolm538 <rmalcolm538@gmail.com>
2021-06-13 18:49:44 -0400

    adding sevanteri fork

--- keyboards/converter/usb_usb/hasu/rules.mk
+++ keyboards/converter/usb_usb/hasu/rules.mk
@@ -3,3 +3,11 @@ F_CPU = 16000000
 
 # Bootloader selection
 BOOTLOADER = atmel-dfu
+
+# Allow combos
+COMBO_ENABLE = no
+
+COMMAND_ENABLE = no
+UNICODE_ENABLE = no
+SWAP_HANDS_ENABLE = no
+MOUSEKEY_ENABLE = no
---
+++ keyboards/converter/usb_usb/keymaps/rmalcolm/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// place overrides here
+
+#define NO_ACTION_LAYER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define DISABLE_LEADER
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
